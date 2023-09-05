d9fe4d8d04e3dd26cb18c7caf7848e7d098cb25f
Guster <spavle.gh@gmail.com>
2023-09-05 11:12:17 +0200

    wcf sept 2023

---
+++ WCF/Septembar2023/IService1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    
+    [ServiceContract]
+    public interface ISistem
+    {
+        [OperationContract]
+        bool Prijavi(Student stud, List<Predmet> predmeti,string rok);
+        [OperationContract]
+        Obavestenje Odjavi(Student stud, Predmet ispit);
+        [OperationContract]
+        List<string> PrijavljeniIspiti(Student stud, string rok);
+        [OperationContract]
+        Hashtable StudenataPoIspitu(string rok);
+        [OperationContract]
+        List<Student> PrijaviliIspit(string rok, Predmet predmet);
+
+    }
+
+    [DataContract]
+    public class Student
+    {
+        int index = 0;
+        string ime = "";
+        string prezime = "";
+
+        [DataMember]
+        public int Index
+        {
+            get { return index; }
+            set { index = value; }
+        }
+
+        [DataMember]
+        public string Ime
+        {
+            get { return ime; }
+            set { ime = value; }
+        }
+        [DataMember]
+        public string Prezime
+        {
+            get { return Prezime; }
+            set { Prezime = value; }
+        }
+
+
+    }
+
+    [DataContract]
+    public class Predmet 
+    {
+        string naziv;
+        string sifra;
+
+        [DataMember]
+        public string Sifra 
+        {
+            get { return sifra; }
+            set { sifra = value; }
+        }
+
+        [DataMember]
+        public string Naziv
+        {
+            get { return naziv; }
+            set { naziv = value; }
+        }
+    }
+
+    [DataContract]
+    public class Prijava {
+
+        Student stud;
+        Predmet pred;
+        string rok;
+
+        [DataMember]
+        public Student Stud
+        {
+            get { return stud; }
+            set { stud = value; }
+        }
+    
+        [DataMember]
+        public Predmet Pred
+        {
+            get { return pred; }
+            set { pred = value; }
+        }
+
+        [DataMember]
+        public string Rok
+        {
+            get { return rok; }
+            set { rok = value; }
+        }
+    }
+
+    [DataContract]
+    public class Obavestenje 
+    {
+        bool success;
+        string error;
+
+        [DataMember]
+        public bool Success
+        {
+            get { return success; }
+            set { success = value; }
+        }
+
+        [DataMember]
+        public string Error
+        {
+            get { return error; }
+            set { error = value; }
+        }
+
+    }
+
+}
---
+++ WCF/Septembar2023/Service1.svc.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService3
+{
+
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+    public class Sistem : ISistem
+    {
+
+        static IList prijave = new List<Prijava>();
+
+        public Obavestenje Odjavi(Student stud, Predmet ispit)
+        {
+            Obavestenje o = new Obavestenje();
+
+            foreach(Prijava p in prijave)
+            {
+                if(p.Stud.Index == stud.Index && ispit.Sifra == p.Pred.Sifra)
+                {
+                    prijave.Remove(p);
+                    o.Success = true;
+                    return o;
+                }
+            }
+            o.Success = false;
+            o.Error = "Nije pronadjena prijava sa navedenim studentom i ispitom";
+            return o;
+        }
+
+        public bool Prijavi(Student stud, List<Predmet> predmeti,string rok)
+        {
+            Prijava pr;
+            foreach (Predmet p in predmeti)
+            {
+                bool postoji = false;
+                foreach(Prijava pri in prijave)
+                {
+                    if(pri.Stud.Index == stud.Index && pri.Pred.Sifra == p.Sifra && pri.Rok == rok)
+                    {
+                        postoji = true;
+                        break;
+                    }
+
+                }
+                if (!postoji) {
+                    pr = new Prijava();
+                    pr.Stud = stud;
+                    pr.Pred = p;
+                    pr.Rok = rok;
+                }
+            }
+
+            return true;
+        }
+
+        public List<Student> PrijaviliIspit(string rok, Predmet predmet)
+        {
+            List<Student> ret = new List<Student>();
+
+            foreach(Prijava p in prijave)
+            {
+                if(p.Pred.Sifra == predmet.Sifra && p.Rok == rok)
+                {
+                    ret.Add(p.Stud);
+                }
+            }
+            return ret;
+        }
+
+        public List<string> PrijavljeniIspiti(Student stud, string rok)
+        {
+            List<string> ret = new List<string>();
+
+            foreach(Prijava p in prijave)
+            {
+                if(p.Stud.Index == stud.Index && p.Rok == rok)
+                {
+                    ret.Add(p.Pred.Naziv);
+                }
+            }
+
+            return ret;
+        }
+
+        public Hashtable StudenataPoIspitu(string rok)
+        {
+
+            Hashtable ret = new Hashtable();
+
+            foreach (Prijava p in prijave)
+            {
+                if (ret.Contains(p.Rok))
+                {
+                    ret[p.Rok] = (int)ret[p.Rok] + 1;
+                }
+                else
+                {
+                    ret.Add(p.Rok,1);
+                }
+            }
+            return ret;
+        }
+
+        
+    }
+}
+
+
+
